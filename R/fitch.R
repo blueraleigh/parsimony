5bd19da4884e74a15c929d1d835892d49e0e14f9
blueraleigh <mcgrundler@gmail.com>
2021-09-21 17:43:54 -0400

    add R documentation files

--- R/fitch.R
+++ R/fitch.R
@@ -38,8 +38,37 @@
 }
 
 
-# Perform a maximum parsimony reconstruction of ancestral
-# states given an r-state character using Fitch's algorithm
+#' Ancestral character state reconstruction using maximum parsimony
+#'
+#' Perform a maximum parsimony reconstruction of ancestral character
+#' states using Fitch's algorithm
+#'
+#' @param phy An object of class \code{tree}.
+#' @param data A named vector of character state data. If \code{levels} is
+#' this should be a \code{factor} or \code{integer} vector.
+#' @param levels A vector containing the different states the character can
+#' take. Optional.
+#' @param ambig A named list containing ambiguous character state encodings.
+#' The names of the list correspond to ambiguous character states. The values
+#' for each list item refer to states in \code{levels}. Thus, if one of the
+#' names in \code{ambig} appears in \code{data}, that data entry is assumed to
+#' potentially map to any of the states in \code{levels} that appear in the
+#' corresponding \code{ambig} entry.
+#' @note A maximum of 31 character states is allowed.
+#' @return A list with five components:
+#' \describe{
+#' \item{score}{The minimum number of character state changes needed to explain
+#' \code{data}.}
+#' \item{scores}{The minimum number of character state changes needed to explain
+#' the data in each clade of \code{phy}. The i-th entry in this array corresponds
+#' to the subtree rooted at the node with index i.}
+#' \item{mpr}{A matrix containing optimal state sets for each internal node.
+#' Each column represents a state and is either 0 (the optimal state set does
+#' not include the state) ) or 1 (the optimal state set includes the state).}
+#' \item{mpr_count}{The number of maximum parsimony reconstructions.}
+#' \item{simulate}{A function to randomly sample maximum parsimony 
+#' reconstructions.}
+#' }
 mpr.fitch = function(phy, data, levels, ambig) {
     stopifnot(is.tree(phy))
     stopifnot(tree.isbinary(phy))
--- R/sankoff.R
+++ R/sankoff.R
@@ -45,8 +45,32 @@
 }
 
 
-# Perform a maximum parsimony reconstruction of ancestral
-# states given an r-state character using Sankoff's algorithm
+#' Ancestral character state reconstruction using maximum parsimony
+#'
+#' Perform a maximum parsimony reconstruction of ancestral character
+#' states using Sankoffs's algorithm
+#'
+#' @param phy An object of class \code{tree}.
+#' @param data A named vector of character state data. If \code{levels} is
+#' this should be a \code{factor} or \code{integer} vector.
+#' @param cost A matrix with state-to-state transition costs.
+#' @param levels A vector containing the different states the character can
+#' take. Optional.
+#' @param ambig A named list containing ambiguous character state encodings.
+#' The names of the list correspond to ambiguous character states. The values
+#' for each list item refer to states in \code{levels}. Thus, if one of the
+#' names in \code{ambig} appears in \code{data}, that data entry is assumed to
+#' potentially map to any of the states in \code{levels} that appear in the
+#' corresponding \code{ambig} entry.
+#' @return A list with three components:
+#' \describe{
+#' \item{mpr}{A matrix containing optimal state sets for each internal node.
+#' Each column represents a state and is either 0 (the optimal state set does
+#' not include the state) ) or 1 (the optimal state set includes the state).}
+#' \item{mpr_count}{The number of maximum parsimony reconstructions.}
+#' \item{simulate}{A function to randomly sample maximum parsimony 
+#' reconstructions.}
+#' }
 mpr.sankoff = function(phy, data, cost, levels, ambig) {
     stopifnot(is.tree(phy))
     stopifnot(!is.null(names(data)))
